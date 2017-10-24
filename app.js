aae526bc3c63f2cfe60f0bfa04e3af6b77a86128
academy-codex <academycodex@gmail.com>
2017-10-25 01:26:26 +0530

    server side streaming added.

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
 const ytdl = require('ytdl-core');
 const fs = require('fs');
+var express = require('express');
 var argv = require('yargs').argv;
 
-let url = argv.url;
-let resolution = argv.res;
+var app = express();
 
-let isValid = ytdl.validateURL(url);
+app.get('/download', function(req, res){
 
-if (isValid) {
-    let videoID = ytdl.getURLVideoID(url);
-    ytdl.getInfo(videoID).then((info) => {
+    console.log("hello");
 
-        let format = ytdl.chooseFormat(info.formats, {
-            resolution: resolution,
-            container: 'mp4'
+    res.setHeader('content-type', 'video/mp4');
+
+    fs.createReadStream('./diwali.mp4').pipe(res);
+});
+
+app.get('/', function(req, res) {
+
+
+    let url = argv.url;
+    let resolution = argv.res;
+
+    let isValid = ytdl.validateURL(url);
+
+    if (isValid) {
+
+        res.setHeader('content-type','video/mp4');
+        let videoID = ytdl.getURLVideoID(url);
+        ytdl.getInfo(videoID).then((info) => {
+
+            let format = ytdl.chooseFormat(info.formats, {
+                resolution: resolution,
+                container: 'mp4'
+            });
+
+            if (format) {
+                console.log("Format Found!");
+                console.log("Download in progress ...");
+
+                let totalSize = format.clen;
+                console.log(format);
+
+                // fs.watchFile(info.title + ' - ' + info.author.user + '.mp4', () => {
+                //     fs.stat(info.title + ' - ' + info.author.user + '.mp4', function (err, stats) {
+                //         console.log((stats.size / totalSize) * 100 + "%");
+                //     });
+                // });
+
+                ytdl(url, {
+                    format: format
+                }).pipe(res);
+
+            } else {
+                console.log("No Format Found!");
+            }
         });
+    } else {
+        res.send("Error");
+        console.log("Invalid Link!");
+    }
 
-        if (format) {
-            console.log("Format Found!");
-            console.log("Download in progress ...");
-            ytdl(url, {
-                format: format
-            }).pipe(fs.createWriteStream(info.title + ' - ' + info.author.user + '.mp4'));
+});
 
-        } else {
-            console.log("No Format Found!");
-        }
-    });
-}else {
-    console.log("Invalid Link!");
-}
+app.listen(3000);
